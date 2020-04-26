1914d4c67e91396ea784f7d0d1ab21445f2a1c76
eastsheng <eastsheng@hotmail.com>
2020-04-26 16:25:36 +0800

    Create setup.py

---
+++ thermal_conductivity/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+	name         = 'thermal_conductivity',
+	version      = '1.2.1',
+	py_modules   = ['thermal_conductivity'],
+	author       = 'DongshengChen',
+	author_email = 'eastsheng@hotmail.com',
+	url          = 'https://github.com/eastsheng',
+	description  = 'Calculate thermal conductivity about NEMD dump.'
+	)
